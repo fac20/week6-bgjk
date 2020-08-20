373c30c5dd4cd4c6798d1d2f778f8c16a646536b
Khadija <khadija.nur99@gmail.com>
2020-08-20 11:39:10 +0100

    Add posts content to database along with user info from cookies

--- src/handlers.js
+++ src/handlers.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const path = require("path");
 const model = require("./model");
 const template = require("./template");
+const { parse } = require("cookie");
+const { sign, verify } = require("jsonwebtoken");
 
 // -------Home Handler------------------
 function home(request, response) {
@@ -17,6 +19,48 @@ function missing(request, response) {
   response.writeHead(200, { "content-type": "text/html" });
   response.end(`<h1>Oops, nothing for you over here</h1>`);
 }
+// --------Create Post Handler---------------
+
+function createPost(request, response) {
+
+
+  let body = "";
+  request.on("data", chunk => (body += chunk));
+  request.on("end", () => {
+    const searchParams = new URLSearchParams(body);
+    const data = Object.fromEntries(searchParams);
+
+    if (!request.headers.cookie) return sendRedirect();
+    const { jwt } = parse(request.headers.cookie);
+    if (!jwt) return sendRedirect();
+
+    return verify(jwt, SECRET, (err, jwt) => {
+      if (err) { return sendRedirect(); }
+      else {
+        let values = [jwt.userID, data.post];
+        model
+          .createPost(values) //save post info to posts table in database
+          .then(() => {
+            response.writeHead(302, { location: "/" });
+            response.end();
+          })
+          .catch(error => {
+            console.error(error);
+            response.writeHead(500, { "content-type": "text/html" });
+            response.end("<h1>No connection no beans</h1>");
+          });
+      }
+      //console.log("data = ", data);
+    });
+
+    // response.writeHead(404, { "content-type": "text/html" });
+    // response.end("<h1>Oops, nothing for you over here</h1>");
+  });
+}
+
+
+
+
 
 // --------Create User Handler---------------
 function createUser(request, response) {
@@ -72,4 +116,4 @@ function public(request, response) {
     }
   });
 }
-module.exports = { home, missing, createUser, public };
+module.exports = { home, missing, createUser, public, createPost };
--- src/model.js
+++ src/model.js
@@ -1,19 +1,15 @@
 const db = require("./database/connection");
+
+
+
+
+
+
 // A query to select all users from the users column
 function getUsers() {
   return db.query("SELECT * FROM users").then(result => result.rows);
 }
-// get posts from posts table
-// function getPosts() {
-//   return db.query("SELECT * FROM posts").then(result => result.rows);
-// }
 
-// Create a function to get data from two different tables(users.username, users.location, posts.text_content)
-
-
-/*SELECT users.username, users.location, posts.textContent 
-FROM users INNER JOIN posts
-ON users.id = posts.user_id */
 
 function getPosts() {
   return db.query(`
@@ -22,13 +18,6 @@ function getPosts() {
     ON users.id = posts.user_id`)
     .then(result => result.rows);
 }
-
-
-
-
-
-
-
 
 // Insert new user into user array
 function createUser(data) {
@@ -42,18 +31,24 @@ function createUser(data) {
     )
     .then(result => result.rows);
 }
+
 // Insert new post into posts table
-function createPost(data) {
-  // Get the primary key of the user and place it in array with the post text content
-  const postValues = [
-    //db.query(`SELECT id FROM users WHERE username = ${data.username}`),
-    createUser(data),
-    data.content,
-  ];
-  // Place the post text into the posts table along side the user_id foreign key
+function createPost(values) {
   return db.query(
-    "INSERT INTO posts(user_id, text_content) VALUES($1, $2)",
-    postValues
+    "INSERT INTO posts(user_id, text_content) VALUES($1, $2)", values
   );
 }
+
+
+//   const postValues = [
+//     //db.query(`SELECT id FROM users WHERE username = ${data.username}`),
+//     createUser(data),
+//     data.content,
+//   ];
+//   // Place the post text into the posts table along side the user_id foreign key
+//   return db.query(
+//     "INSERT INTO posts(user_id, text_content) VALUES($1, $2)",
+//     postValues
+//   );
+// }
 module.exports = { getUsers, createUser, createPost, getPosts };
